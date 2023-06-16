77ea61fbf240bc24a3ab59deeb8445ed6b57ffb8
chenxiaoming <732803261@qq.com>
2023-06-16 17:37:20 +0800

    update

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ bot = commands.Bot(command_prefix='/')
 
 @app.route('/', methods=['GET', 'POST'])
 def midjourney():
-    if request.method == 'POST':
+    if 1==1:
         prompt = request.json['prompt']
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
